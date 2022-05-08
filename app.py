1fe81f2ee62b1934277c8a602d0f04f3be1dde90
Mathenge-Alex <arxmaina@gmail.com>
2022-05-08 17:06:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# from flask import Flask, Blueprint
+# from .main.views import main
+# def create_app():
+#     app = Flask(__name__)
+
+#     app.register_blueprint(main)
+
+#     return app
+
+# # breakpoint()
