ce18080109042416a17c1c5995b8cedabfc88430
Mathenge-Alex <arxmaina@gmail.com>
2022-05-08 17:22:43 +0300

    Initial test

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-# from flask import Flask, Blueprint
+from flask import Flask, Blueprint
 # from .main.views import main
 # def create_app():
-#     app = Flask(__name__)
+app = Flask(__name__)
 
 #     app.register_blueprint(main)
 
 #     return app
 
 # # breakpoint()
+
+if __name__=="__main__":
+    app.run(debug=True)
