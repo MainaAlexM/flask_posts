2af2422389fa55f60c752b39f6876775955694c0
Mathenge-Alex <arxmaina@gmail.com>
2022-05-09 06:16:06 +0300

    feat: add class comments

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from email.policy import default
+from ..app import db
+
+class Comments(db.model):
+    id = db.Column(db.Interger, primary_key = True)
+    title = db.Column(db.string(100), nullable=False)
+    content = db.Column(db.text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
