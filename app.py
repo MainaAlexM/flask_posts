b4dd7f0d2ab83b0a5e3b028127d0db22b901f33f
Mathenge-Alex <arxmaina@gmail.com>
2022-05-09 04:44:12 +0300

    load sqlalchemy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
 # from .main.views import main
 # def create_app():
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
 #     app.register_blueprint(main)
 
