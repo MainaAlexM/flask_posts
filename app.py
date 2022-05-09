8f20719584b1ac7daecfea5531badb3aa2a9c496
Mathenge-Alex <arxmaina@gmail.com>
2022-05-09 06:37:26 +0300

    fix: fix typing errors in database creation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 # from .main.views import main
@@ -5,6 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///posts.db'
 db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Comment' + str(self.id)
+# from .main import models
+
 
 #     app.register_blueprint(main)
 
--- main/models.py
+++ main/models.py
@@ -1,13 +1,13 @@
-from datetime import datetime
-from email.policy import default
-from ..app import db
+# from datetime import datetime
+# from email.policy import default
+# from app import db
 
-class Comments(db.model):
-    id = db.Column(db.Interger, primary_key = True)
-    title = db.Column(db.string(100), nullable=False)
-    content = db.Column(db.text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+# class Comments(db.model):
+#     id = db.Column(db.Interger, primary_key = True)
+#     title = db.Column(db.string(100), nullable=False)
+#     content = db.Column(db.text, nullable=False)
+#     author = db.Column(db.String(20), nullable=False, default='N/A')
+#     date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
 
-    def __repr__(self):
-        return 'Comment' + str(self.id)
+#     def __repr__(self):
+#         return 'Comment' + str(self.id)
