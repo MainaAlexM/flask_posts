7e2e12f62203e43be3a940200e3d766c4c7bca64
Mathenge-Alex <arxmaina@gmail.com>
2022-05-12 07:27:13 +0300

    add Pitch and Comment Form

---
+++ main/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import Required
+from wtforms import TextAreaField, StringField, SelectField, SubmitField
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Title',validators=[Required()])
+    text = TextAreaField('Your Pitch',validators=[Required()])
+    category = SelectField('Type',choices=[('Business','Business pitch'),('Work','Work pitch'),('Sales','Sales pitch')],validators=[Required()])
+    submit = SubmitField('Post Pitch')
+
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment:',validators=[Required()])
+    submit = SubmitField('Post Comment')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio.',validators = [Required()])
+    submit = SubmitField('Update')
