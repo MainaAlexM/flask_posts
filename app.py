033e8af18b444957e33039b91bde0e25665c20e6
Mathenge-Alex <arxmaina@gmail.com>
2022-05-15 23:20:18 +0300

    refactor: refactor application code to fix bugs

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from application import create_app,db
-from flask_script import Manager,Server
-from application.models import User
-from flask_migrate import Migrate, MigrateCommand
+# from application import create_app,db
+# from flask_script import Manager,Server
+# from application.models import User
+# from flask_migrate import Migrate, MigrateCommand
+from application import app
 
 #Creating app instance
-app = create_app('production')
+# app = create_app('production')
 
-manager = Manager(app)
-manager.add_command('server',Server)
+# manager = Manager(app)
+# manager.add_command('server',Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+# migrate = Migrate(app,db)
+# manager.add_command('db',MigrateCommand)
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User)
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
